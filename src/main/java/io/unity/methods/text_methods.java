00f3209d40a677d41119cd75444a6b04e2fdf9d3
Nishil Patel <nishilpatel@cloud.upwork.com>
2021-12-20 19:14:53 +0530

    make methods public

--- src/main/java/io/unity/methods/text_methods.java
+++ src/main/java/io/unity/methods/text_methods.java
@@ -15,7 +15,7 @@ public class text_methods {
 
     public void verify_text()
     {
-        java_class.addMethod().setName("click_on_" + key_name + "_button").setBody("verify.element_text_is_equal_to(\""+key_name+"\",button_text);").setParameters("String button_text");
+        java_class.addMethod().setName("click_on_" + key_name + "_button").setBody("verify.element_text_is_equal_to(\""+key_name+"\",button_text);").setParameters("String button_text").setPublic();
     }
 
 
