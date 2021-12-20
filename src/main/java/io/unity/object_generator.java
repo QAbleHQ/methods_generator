10d75c081492f31d5402389d45dc78e1d182bc21
Nishil Patel <nishilpatel@cloud.upwork.com>
2021-12-20 19:01:25 +0530

    Updated Code

--- src/main/java/io/unity/object_generator.java
+++ src/main/java/io/unity/object_generator.java
@@ -27,12 +27,12 @@ public class object_generator {
             System.out.println("Enter The destination Folder path");
             String destination_path = scanner.nextLine();
 
-            System.out.println("Enter The destination path");
+            System.out.println("Enter Class Name (Without Any Extension)");
             String destination_class_name = scanner.nextLine();
 
             JavaClassSource java_class = builder.build_object_repo_class(destination_class_name, builder.read_locator_file_and_get_object(file_path));
 
-            builder.write_java_file(destination_path+"/"+java_class,java_class);
+            builder.write_java_file(destination_path+"/"+destination_class_name+".java",java_class);
 
         } catch (Exception e) {
             e.printStackTrace();
