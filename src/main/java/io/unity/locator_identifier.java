31918f5b91a3faadf00bbebe42439d86dddbadfa
Nishil Patel <nishilpatel@cloud.upwork.com>
2021-12-20 18:11:55 +0530

    Added text methods

--- src/main/java/io/unity/locator_identifier.java
+++ src/main/java/io/unity/locator_identifier.java
@@ -2,6 +2,7 @@ package io.unity;
 
 import io.unity.methods.button_methods;
 import io.unity.methods.text_box_methods;
+import io.unity.methods.text_methods;
 import org.jboss.forge.roaster.model.source.JavaClassSource;
 import org.json.simple.JSONObject;
 
@@ -17,7 +18,8 @@ public class locator_identifier {
     public void identify_locator_and_generate_object(String key_name, JSONObject single_object) {
 
         text_box_methods text_box_method = new text_box_methods(java_class);
-        button_methods button_methods = new button_methods(java_class);
+        button_methods button_methods = new button_methods(java_class, key_name);
+        text_methods text_methods = new text_methods(java_class, key_name);
 
         String element_type = (String) single_object.get("element_type");
         if (element_type.equals("text_box")) {
@@ -28,29 +30,41 @@ public class locator_identifier {
             text_box_method.clear_and_enter_text_box(key_name);
 
         } else if (element_type.equals("email")) {
+            /*
+            * Common Methods of the text Box
+            * */
+            text_box_method.enter_text(key_name);
+            text_box_method.clear_text_box(key_name);
+            text_box_method.verify_text_box_is_present(key_name);
+            text_box_method.clear_and_enter_text_box(key_name);
 
+            /*
+            * Add Some Email Related Methods
+            * */
         } else if (element_type.equals("password")) {
 
         } else if (element_type.equals("button")) {
-            button_methods.verify_button_is_present(key_name);
+            button_methods.verify_button_is_present();
+            button_methods.click_on_button();
+            button_methods.verify_button_is_clickable();
+            button_methods.verify_button_text();
 
         } else if (element_type.equals("check_box")) {
 
 
-        } else if (element_type.equals("label")) {
+        } else if (element_type.equals("text")) {
+            text_methods.verify_text();
 
 
         } else if (element_type.equals("radio")) {
 
-        }
-        else if (element_type.equals("text_area")) {
+        } else if (element_type.equals("text_area")) {
 
-        }
-        else if (element_type.equals("drop_down")) {
+        } else if (element_type.equals("drop_down")) {
 
-        }else if (element_type.equals("file")) {
+        } else if (element_type.equals("file")) {
 
-        }else if (element_type.equals("link")) {
+        } else if (element_type.equals("link")) {
 
         }
 
---
+++ src/main/java/io/unity/methods/text_methods.java
@@ -0,0 +1,25 @@
+package io.unity.methods;
+
+import org.jboss.forge.roaster.model.source.JavaClassSource;
+
+public class text_methods {
+
+    JavaClassSource java_class;
+    String key_name;
+
+    public text_methods(JavaClassSource java_class, String key_name) {
+        this.java_class = java_class;
+        this.key_name = key_name;
+    }
+
+
+    public void verify_text()
+    {
+        java_class.addMethod().setName("click_on_" + key_name + "_button").setBody("verify.element_text_is_equal_to(\""+key_name+"\",button_text);").setParameters("String button_text");
+    }
+
+
+
+
+
+}
